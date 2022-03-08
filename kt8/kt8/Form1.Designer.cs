0ca80526ba5708de50728d2ac56e65b0351e186a
Nhut Phan <phanthanhnhut2018@outlook.com>
2022-03-08 08:53:20 +0700

    commit

--- kt8/kt8/Form1.Designer.cs
+++ kt8/kt8/Form1.Designer.cs
@@ -29,13 +29,44 @@ namespace kt8
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.UserName = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // UserName
+            // 
+            this.UserName.AutoSize = true;
+            this.UserName.Location = new System.Drawing.Point(66, 75);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(75, 17);
+            this.UserName.TabIndex = 0;
+            this.UserName.Text = "UserName";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(250, 70);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(275, 22);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(998, 536);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.UserName);
+            this.Name = "Form1";
+            this.Text = "UserName";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label UserName;
+        private System.Windows.Forms.TextBox txtUserName;
     }
 }
 
